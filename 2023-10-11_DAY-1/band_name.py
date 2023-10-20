3550527ee6b2d3e54579dcf94f1e7cc902c14223
sarathss100 <sarathss100@gmail.com>
2023-10-20 19:41:54 +0530

    2023-10-11_DAY-1

---
+++ 2023-10-11_DAY-1/band_name.py
@@ -0,0 +1,9 @@
+print("Welcome to the band name Generator")
+
+city = input("What's the name of the city you lived in?\n")
+
+pet_name = input("What is your pet name?\n")
+
+band_name = city+pet_name
+
+print("Your band name is ",band_name)
