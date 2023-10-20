17061c76a47373a027875d7c9bba946508dc9c6f
sarathss100 <sarathss100@gmail.com>
2023-10-20 19:44:17 +0530

    2023-10-13-DAY-2

---
+++ 2023-10-13-DAY-2/main.py
@@ -0,0 +1,7 @@
+# num = input("Please enter a 2 digit number!\n")
+# num_char = str(num)
+# num1 = num_char[0]
+# num2 = num_char[1]
+# sum = int(num1) + int(num2)
+# print("Sum of 2 digits is " + str(sum))
+print(3 * 3 + 3 / 3 - 3)
