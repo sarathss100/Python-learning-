1e545b0ecc4ac6f01dd4f385fc1967c22ecde4dd
sarathss100 <sarathss100@gmail.com>
2023-10-20 19:46:23 +0530

    2023-10-18-DAY-3

---
+++ 2023-10-18-DAY-3/age.py
@@ -0,0 +1,8 @@
+print("Welcome to Roller coaster")
+
+height = int(input("Please enter your height in cm : "))
+
+if height > 120:
+    print("You are eligible for this ride")
+else:
+    print("You are not eligible for this ride")
