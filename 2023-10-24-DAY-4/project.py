d30d2744908e0c9bdb73cfe989f69be7cc59313c
sarathss100 <sarathss100@gmail.com>
2023-10-24 18:25:33 +0530

    2023-10-24-DAY-4

---
+++ 2023-10-24-DAY-4/project.py
@@ -0,0 +1,49 @@
+import random
+
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+game = [rock, paper, scissors]
+option = int(input("Please choose '0' for rock, '1' for paper, '2' scissor!\n"))
+
+print(f"You choose : \n{game[option]}")
+
+computer_option = random.choice(game)
+print(f"Computer choose : \n{computer_option}")
+computer_option_index = game.index(computer_option)
+
+if option == computer_option_index:
+    print("Invalid")
+elif option == 0 and computer_option_index == 2:
+    print("You Won!")
+elif option == 1 and computer_option_index == 0:
+    print("You Won!")
+elif option == 2 and computer_option_index == 1:
+    print("You Won!")
+else:
+    print("Computer Won!")
+
