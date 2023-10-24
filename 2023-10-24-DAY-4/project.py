2244c5ba320a7a386073cf5c2667271e44530118
sarathss100 <sarathss100@gmail.com>
2023-10-24 18:53:33 +0530

    2023-10-24-DAY-4

--- 2023-10-24-DAY-4/project.py
+++ 2023-10-24-DAY-4/project.py
@@ -30,20 +30,26 @@ scissors = '''
 game = [rock, paper, scissors]
 option = int(input("Please choose '0' for rock, '1' for paper, '2' scissor!\n"))
 
-print(f"You choose : \n{game[option]}")
-
-computer_option = random.choice(game)
-print(f"Computer choose : \n{computer_option}")
-computer_option_index = game.index(computer_option)
-
-if option == computer_option_index:
-    print("Invalid")
-elif option == 0 and computer_option_index == 2:
-    print("You Won!")
-elif option == 1 and computer_option_index == 0:
-    print("You Won!")
-elif option == 2 and computer_option_index == 1:
-    print("You Won!")
+if option <= 2 and option >= 0:
+    print(f"You choose : \n{game[option]}")
+    computer_option = random.choice(game)
+    computer_option_index = game.index(computer_option)
+    print(f"Computer choose : \n{computer_option}")
+    if option == computer_option_index:
+        print("Draw!")
+    elif option > 2:
+        print("Invalid")
+    elif option == 0 and computer_option_index == 2:
+        print("You Won!")
+    elif option == 1 and computer_option_index == 0:
+        print("You Won!")
+    elif option == 2 and computer_option_index == 1:
+        print("You Won!")
+    else:
+        print("Computer Won!")
 else:
-    print("Computer Won!")
+   print("Invalid number!") 
+    
+
+
 
